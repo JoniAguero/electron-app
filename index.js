bab01920d0a6507e4e602709b51e42193cf72550
JoniAguero <jaguero@trimix.com.ar>
2018-11-07 22:55:47 -0300

    BrowserWindow
    Permite crear la ventana de electron donde se mostrara la app que se desarrollara

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 'use strict'
 
-const { app } = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+app.on('before-quit', () => {
+    console.log('saliendo');
+});
+
+app.on('ready', () => {
+    let win = new BrowserWindow();
+    win.on('closed', () => {
+        win = null;
+        app.quit();
+    })
+})
