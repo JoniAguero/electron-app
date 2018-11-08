1d14b78950ee80a419ef25328c6f8d4816372779
JoniAguero <jaguero@trimix.com.ar>
2018-11-08 08:28:51 -0300

    Filtro de Busqueda

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,7 +1,8 @@
-import os from 'os';
+import url from 'url';
 
 window.addEventListener('load', () => {
-    addImageEvents()
+    addImageEvents();
+    searchImagesEvent();
 })
 
 function addImageEvents () {
@@ -19,4 +20,29 @@ function changeImage (node) {
     document.querySelector('li.selected').classList.remove('selected');
     node.classList.add('selected');
     document.getElementById('image-displayed').src = node.querySelector('img').src;
+}
+
+function searchImagesEvent() {
+    const searchBox = document.getElementById('search-box');
+    searchBox.addEventListener('keyup', function () {
+        const regex = new RegExp(this.value.toLowerCase(), 'gi');
+        if (this.value.length > 0) {
+            const thumbs = document.querySelectorAll('li.list-group-item img')
+            for (let i = 0; i < thumbs.length; i++) {
+                const fileUrl = url.parse(thumbs[i].src)
+                const fileName = path.basename(fileUrl.pathname)
+                if (fileName.match(regex)) {
+                    thumbs[i].parentNode.classList.remove('hidden')
+                } else {
+                    thumbs[i].parentNode.classList.add('hidden')
+                }
+            }
+            selectFirstImage()
+        }
+    })
+}
+
+function selectFirstImage () {
+    const image = document.querySelector('li.list-group-item:not(.hidden)')
+    changeImage(image)
 }
