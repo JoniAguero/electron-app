9f299f8d996d4ba070120b6f52ae0d50f18c3f66
JoniAguero <jaguero@trimix.com.ar>
2018-11-08 08:07:28 -0300

    Evento Seleccion de Imagen

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,4 +1,22 @@
 import os from 'os';
+
 window.addEventListener('load', () => {
-    console.log(os.cpus());
+    addImageEvents()
 })
+
+function addImageEvents () {
+    const thumbs = document.querySelectorAll('li.list-group-item');
+
+    thumbs.forEach(function (element) {
+        element.addEventListener('click', () => {
+            changeImage(element)
+        })
+    })
+
+}
+
+function changeImage (node) {
+    document.querySelector('li.selected').classList.remove('selected');
+    node.classList.add('selected');
+    document.getElementById('image-displayed').src = node.querySelector('img').src;
+}
